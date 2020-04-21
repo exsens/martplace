5df791d3c4b51b88158e42528b0f287b4e841a3b
exsens <knyazex@mail.ru>
2020-04-21 20:56:32 +0300

    added single page

--- app/js/main.js
+++ app/js/main.js
@@ -62,6 +62,20 @@ $(function(){
          }
          
       });
+      
+      $('.control').on('change',function(){
+        if($(this).prop('checked')){
+            $('.check.'+$(this).attr('data-type')).prop('checked',true);
+        }
+    })
+      $('.single-content__tabs .tab').on('click', function(event) {
+        var id = $(this).attr('data-id');
+          $('.single-content__tabs').find('.tab-item').removeClass('active-tab').hide();
+          $('.single-content__tabs .tabs').find('.tab').removeClass('active');
+          $(this).addClass('active');
+          $('#'+id).addClass('active-tab').fadeIn();
+          return false;
+        });
     $('select').styler();
 
     $('.rating').rateYo({
